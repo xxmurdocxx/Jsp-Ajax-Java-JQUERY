7f6ed775943ebf86a8ec05327fdf656ac235552f
Aquineche <albertoquineche86@gmail.com>
2020-01-21 18:22:18 -0500

    Cambios Aq

--- src/ua/vadixem/ajax/servlets/UpdateUsername.java
+++ src/ua/vadixem/ajax/servlets/UpdateUsername.java
@@ -32,7 +32,7 @@ public class UpdateUsername extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	//Avanzar la logica aqui	
 	}
 
 	/**
